50daf361474738b7e0d9766785266ba898820f3a
Kevin Eshleman <hello@redkevin.com>
2019-01-04 13:23:59 -0600

    build 4.9.2-mysw-9

--- js/tinymce/plugins/anchor/plugin.js
+++ js/tinymce/plugins/anchor/plugin.js
@@ -1,118 +1,118 @@
 (function () {
 var anchor = (function () {
-  'use strict';
+    'use strict';
 
-  var global = tinymce.util.Tools.resolve('tinymce.PluginManager');
+    var global = tinymce.util.Tools.resolve('tinymce.PluginManager');
 
-  var isValidId = function (id) {
-    return /^[A-Za-z][A-Za-z0-9\-:._]*$/.test(id);
-  };
-  var getId = function (editor) {
-    var selectedNode = editor.selection.getNode();
-    var isAnchor = selectedNode.tagName === 'A' && editor.dom.getAttrib(selectedNode, 'href') === '';
-    return isAnchor ? selectedNode.id || selectedNode.name : '';
-  };
-  var insert = function (editor, id) {
-    var selectedNode = editor.selection.getNode();
-    var isAnchor = selectedNode.tagName === 'A' && editor.dom.getAttrib(selectedNode, 'href') === '';
-    if (isAnchor) {
-      selectedNode.removeAttribute('name');
-      selectedNode.id = id;
-      editor.undoManager.add();
-    } else {
-      editor.focus();
-      editor.selection.collapse(true);
-      editor.execCommand('mceInsertContent', false, editor.dom.createHTML('a', { id: id }));
-    }
-  };
-  var $_fd84tv8njm9dk6p6 = {
-    isValidId: isValidId,
-    getId: getId,
-    insert: insert
-  };
-
-  var insertAnchor = function (editor, newId) {
-    if (!$_fd84tv8njm9dk6p6.isValidId(newId)) {
-      editor.windowManager.alert('Id should start with a letter, followed only by letters, numbers, dashes, dots, colons or underscores.');
-      return true;
-    } else {
-      $_fd84tv8njm9dk6p6.insert(editor, newId);
-      return false;
-    }
-  };
-  var open = function (editor) {
-    var currentId = $_fd84tv8njm9dk6p6.getId(editor);
-    editor.windowManager.open({
-      title: 'Anchor',
-      body: {
-        type: 'textbox',
-        name: 'id',
-        size: 40,
-        label: 'Id',
-        value: currentId
-      },
-      onsubmit: function (e) {
-        var newId = e.data.id;
-        if (insertAnchor(editor, newId)) {
-          e.preventDefault();
-        }
-      }
-    });
-  };
-  var $_78fcru8mjm9dk6p5 = { open: open };
-
-  var register = function (editor) {
-    editor.addCommand('mceAnchor', function () {
-      $_78fcru8mjm9dk6p5.open(editor);
-    });
-  };
-  var $_cnj5so8ljm9dk6p4 = { register: register };
-
-  var isAnchorNode = function (node) {
-    return !node.attr('href') && (node.attr('id') || node.attr('name')) && !node.firstChild;
-  };
-  var setContentEditable = function (state) {
-    return function (nodes) {
-      for (var i = 0; i < nodes.length; i++) {
-        if (isAnchorNode(nodes[i])) {
-          nodes[i].attr('contenteditable', state);
-        }
+    var isValidId = function (id) {
+      return /^[A-Za-z][A-Za-z0-9\-:._]*$/.test(id);
+    };
+    var getId = function (editor) {
+      var selectedNode = editor.selection.getNode();
+      var isAnchor = selectedNode.tagName === 'A' && editor.dom.getAttrib(selectedNode, 'href') === '';
+      return isAnchor ? selectedNode.id || selectedNode.name : '';
+    };
+    var insert = function (editor, id) {
+      var selectedNode = editor.selection.getNode();
+      var isAnchor = selectedNode.tagName === 'A' && editor.dom.getAttrib(selectedNode, 'href') === '';
+      if (isAnchor) {
+        selectedNode.removeAttribute('name');
+        selectedNode.id = id;
+        editor.undoManager.add();
+      } else {
+        editor.focus();
+        editor.selection.collapse(true);
+        editor.execCommand('mceInsertContent', false, editor.dom.createHTML('a', { id: id }));
       }
     };
-  };
-  var setup = function (editor) {
-    editor.on('PreInit', function () {
-      editor.parser.addNodeFilter('a', setContentEditable('false'));
-      editor.serializer.addNodeFilter('a', setContentEditable(null));
-    });
-  };
-  var $_ayl6o58ojm9dk6p7 = { setup: setup };
+    var Anchor = {
+      isValidId: isValidId,
+      getId: getId,
+      insert: insert
+    };
 
-  var register$1 = function (editor) {
-    editor.addButton('anchor', {
-      icon: 'anchor',
-      tooltip: 'Anchor',
-      cmd: 'mceAnchor',
-      stateSelector: 'a:not([href])'
-    });
-    editor.addMenuItem('anchor', {
-      icon: 'anchor',
-      text: 'Anchor',
-      context: 'insert',
-      cmd: 'mceAnchor'
-    });
-  };
-  var $_c01ieh8pjm9dk6p8 = { register: register$1 };
+    var insertAnchor = function (editor, newId) {
+      if (!Anchor.isValidId(newId)) {
+        editor.windowManager.alert('Id should start with a letter, followed only by letters, numbers, dashes, dots, colons or underscores.');
+        return true;
+      } else {
+        Anchor.insert(editor, newId);
+        return false;
+      }
+    };
+    var open = function (editor) {
+      var currentId = Anchor.getId(editor);
+      editor.windowManager.open({
+        title: 'Anchor',
+        body: {
+          type: 'textbox',
+          name: 'id',
+          size: 40,
+          label: 'Id',
+          value: currentId
+        },
+        onsubmit: function (e) {
+          var newId = e.data.id;
+          if (insertAnchor(editor, newId)) {
+            e.preventDefault();
+          }
+        }
+      });
+    };
+    var Dialog = { open: open };
 
-  global.add('anchor', function (editor) {
-    $_ayl6o58ojm9dk6p7.setup(editor);
-    $_cnj5so8ljm9dk6p4.register(editor);
-    $_c01ieh8pjm9dk6p8.register(editor);
-  });
-  function Plugin () {
-  }
+    var register = function (editor) {
+      editor.addCommand('mceAnchor', function () {
+        Dialog.open(editor);
+      });
+    };
+    var Commands = { register: register };
 
-  return Plugin;
+    var isAnchorNode = function (node) {
+      return !node.attr('href') && (node.attr('id') || node.attr('name')) && !node.firstChild;
+    };
+    var setContentEditable = function (state) {
+      return function (nodes) {
+        for (var i = 0; i < nodes.length; i++) {
+          if (isAnchorNode(nodes[i])) {
+            nodes[i].attr('contenteditable', state);
+          }
+        }
+      };
+    };
+    var setup = function (editor) {
+      editor.on('PreInit', function () {
+        editor.parser.addNodeFilter('a', setContentEditable('false'));
+        editor.serializer.addNodeFilter('a', setContentEditable(null));
+      });
+    };
+    var FilterContent = { setup: setup };
+
+    var register$1 = function (editor) {
+      editor.addButton('anchor', {
+        icon: 'anchor',
+        tooltip: 'Anchor',
+        cmd: 'mceAnchor',
+        stateSelector: 'a:not([href])'
+      });
+      editor.addMenuItem('anchor', {
+        icon: 'anchor',
+        text: 'Anchor',
+        context: 'insert',
+        cmd: 'mceAnchor'
+      });
+    };
+    var Buttons = { register: register$1 };
+
+    global.add('anchor', function (editor) {
+      FilterContent.setup(editor);
+      Commands.register(editor);
+      Buttons.register(editor);
+    });
+    function Plugin () {
+    }
+
+    return Plugin;
 
 }());
 })();
--- js/tinymce/plugins/code/plugin.js
+++ js/tinymce/plugins/code/plugin.js
@@ -1,94 +1,94 @@
 (function () {
 var code = (function () {
-  'use strict';
+    'use strict';
 
-  var global = tinymce.util.Tools.resolve('tinymce.PluginManager');
+    var global = tinymce.util.Tools.resolve('tinymce.PluginManager');
 
-  var global$1 = tinymce.util.Tools.resolve('tinymce.dom.DOMUtils');
+    var global$1 = tinymce.util.Tools.resolve('tinymce.dom.DOMUtils');
 
-  var getMinWidth = function (editor) {
-    return editor.getParam('code_dialog_width', 600);
-  };
-  var getMinHeight = function (editor) {
-    return editor.getParam('code_dialog_height', Math.min(global$1.DOM.getViewPort().h - 200, 500));
-  };
-  var $_fm4txea1jm9dk6ua = {
-    getMinWidth: getMinWidth,
-    getMinHeight: getMinHeight
-  };
+    var getMinWidth = function (editor) {
+      return editor.getParam('code_dialog_width', 600);
+    };
+    var getMinHeight = function (editor) {
+      return editor.getParam('code_dialog_height', Math.min(global$1.DOM.getViewPort().h - 200, 500));
+    };
+    var Settings = {
+      getMinWidth: getMinWidth,
+      getMinHeight: getMinHeight
+    };
 
-  var setContent = function (editor, html) {
-    editor.focus();
-    editor.undoManager.transact(function () {
-      editor.setContent(html);
+    var setContent = function (editor, html) {
+      editor.focus();
+      editor.undoManager.transact(function () {
+        editor.setContent(html);
+      });
+      editor.selection.setCursorLocation();
+      editor.nodeChanged();
+    };
+    var getContent = function (editor) {
+      return editor.getContent({ source_view: true });
+    };
+    var Content = {
+      setContent: setContent,
+      getContent: getContent
+    };
+
+    var open = function (editor) {
+      var minWidth = Settings.getMinWidth(editor);
+      var minHeight = Settings.getMinHeight(editor);
+      var win = editor.windowManager.open({
+        title: 'Source code',
+        body: {
+          type: 'textbox',
+          name: 'code',
+          multiline: true,
+          minWidth: minWidth,
+          minHeight: minHeight,
+          spellcheck: false,
+          style: 'direction: ltr; text-align: left'
+        },
+        onSubmit: function (e) {
+          Content.setContent(editor, e.data.code);
+        }
+      });
+      win.find('#code').value(Content.getContent(editor));
+    };
+    var Dialog = { open: open };
+
+    var register = function (editor) {
+      editor.addCommand('mceCodeEditor', function () {
+        Dialog.open(editor);
+      });
+    };
+    var Commands = { register: register };
+
+    var register$1 = function (editor) {
+      editor.addButton('code', {
+        icon: 'code',
+        tooltip: 'Source code',
+        onclick: function () {
+          Dialog.open(editor);
+        }
+      });
+      editor.addMenuItem('code', {
+        icon: 'code',
+        text: 'Source code',
+        onclick: function () {
+          Dialog.open(editor);
+        }
+      });
+    };
+    var Buttons = { register: register$1 };
+
+    global.add('code', function (editor) {
+      Commands.register(editor);
+      Buttons.register(editor);
+      return {};
     });
-    editor.selection.setCursorLocation();
-    editor.nodeChanged();
-  };
-  var getContent = function (editor) {
-    return editor.getContent({ source_view: true });
-  };
-  var $_2fco3ka3jm9dk6uc = {
-    setContent: setContent,
-    getContent: getContent
-  };
+    function Plugin () {
+    }
 
-  var open = function (editor) {
-    var minWidth = $_fm4txea1jm9dk6ua.getMinWidth(editor);
-    var minHeight = $_fm4txea1jm9dk6ua.getMinHeight(editor);
-    var win = editor.windowManager.open({
-      title: 'Source code',
-      body: {
-        type: 'textbox',
-        name: 'code',
-        multiline: true,
-        minWidth: minWidth,
-        minHeight: minHeight,
-        spellcheck: false,
-        style: 'direction: ltr; text-align: left'
-      },
-      onSubmit: function (e) {
-        $_2fco3ka3jm9dk6uc.setContent(editor, e.data.code);
-      }
-    });
-    win.find('#code').value($_2fco3ka3jm9dk6uc.getContent(editor));
-  };
-  var $_7j4n18a0jm9dk6u9 = { open: open };
-
-  var register = function (editor) {
-    editor.addCommand('mceCodeEditor', function () {
-      $_7j4n18a0jm9dk6u9.open(editor);
-    });
-  };
-  var $_brfyyk9zjm9dk6u8 = { register: register };
-
-  var register$1 = function (editor) {
-    editor.addButton('code', {
-      icon: 'code',
-      tooltip: 'Source code',
-      onclick: function () {
-        $_7j4n18a0jm9dk6u9.open(editor);
-      }
-    });
-    editor.addMenuItem('code', {
-      icon: 'code',
-      text: 'Source code',
-      onclick: function () {
-        $_7j4n18a0jm9dk6u9.open(editor);
-      }
-    });
-  };
-  var $_55bx5ia4jm9dk6ud = { register: register$1 };
-
-  global.add('code', function (editor) {
-    $_brfyyk9zjm9dk6u8.register(editor);
-    $_55bx5ia4jm9dk6ud.register(editor);
-    return {};
-  });
-  function Plugin () {
-  }
-
-  return Plugin;
+    return Plugin;
 
 }());
 })();
